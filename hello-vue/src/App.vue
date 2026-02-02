8b3ca5fbf195006c4364f64c8c6f1bba15569721
parksw <home@naver.com>
2026-02-02 22:02:51 +0900

    0202

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -9,7 +9,7 @@ import TestComponent from './components/TestComponent.vue';
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
+      <HelloWorld msg="You did it!" />
        <TestComponent />
     </div>
   </header>
